d94a03d3c14cfd7abec824de54c3babd861d5ef7
donnior <donnior@gmail.com>
2012-11-15 13:11:41 +0800

    clean code.

--- src/main/webapp/resources/admin/js/application.js
+++ src/main/webapp/resources/admin/js/application.js
@@ -1,17 +1,7 @@
-
-    
-// $(function() {
-//         $( "#sortable" ).sortable();
-//         $( "#sortable" ).disableSelection();
-//     });
-
-               
 $(function() {
 
   $('#hotDaysHint').popover();
   $('.datepicker').pikaday({ firstDay: 1, format: 'YYYY-MM-DD'});
-
-
 
   // $("#sortableMenu").sortable({ opacity: 0.6, cursor: 'move', placeholder: "ui-state-highlight", update: function() {
   $("#sortableMenu").sortable({ opacity: 0.6, cursor: 'move', update: function() {
@@ -22,7 +12,7 @@ $(function() {
     // $.post("updateDB.php", order, function(theResponse){
     //   $("#contentRight").html(theResponse);
     // });
-  }                 
+    }                 
   });
 
 
